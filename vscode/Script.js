80acd109feff12b98ec73c8cc5ff6610d3a8bc93
Phonrathana.github.io <127619322+phonRathana@users.noreply.github.com>
2023-09-14 01:41:45 +0700

    Update Script.js

--- vscode/Script.js
+++ vscode/Script.js
@@ -4,36 +4,12 @@
      function  signIn () {
   var auth2 = gapi.auth2.getAuthInstance();
 
-  // Sign-In
-  auth2.signIn()
-  .then(authenticateWithServer)
-  .then(changeProfile, function(error) {
-    app.fire('show-toast', {
-      text: 'Authentication failed.'
-    });
-  });
-};
-/**
- * Trigger sign-out using Google Sign-In
- */
-   function signOut () {
-  // Get `GoogleAuth` instance
-  var auth2 = gapi.auth2.getAuthInstance();
-
-  // Sign-Out
-  fetch('/signout', {
-    method: 'POST',
-    credentials: 'include'
-  }).then(function(resp) {
-    if (resp.status === 200) {
-      auth2.signOut()
-      .then(changeProfile);
-    } else {
-      throw "Couldn't sign out";
+ <script type="text/javascript">
+           
+        function signIn ()
+          {
+           document.getElementById("signin").classList.toggle("active");
+           document.getElementById("ul").classList.toggle("link");
+       }
     }
-  }).catch(function(error) {
-    app.fire('show-toast', {
-      text: error
-    });
-  });
-};
+}
