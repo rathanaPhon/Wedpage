fba82b7a15fd73bb416e162a8b96ec37e40550b8
Rathana.github.io.git <153509354+rathanaPhon@users.noreply.github.com>
2024-02-04 00:08:07 +0700

    Update Package.js
    Signed-off-by: Rathana.github.io.git <153509354+rathanaPhon@users.noreply.github.com>

--- JavaScript/Package.js
+++ JavaScript/Package.js
@@ -1,16 +1,18 @@
  <script src="https://rathanaphon.github.io/wedpage/"></script> 
- function onSignIn(googleUser) { 
-   var profile = googleUser.getBasicProfile(); 
-   console.log('ID: ' + profile.getId());  
-   console.log('Name: ' + profile.getName()); 
-   console.log('Image URL: ' + profile.getImageUrl()); 
-   console.log('Email: ' + profile.getEmail());  
-
- <button id onclick="signin();">Signin</button> 
-   function signin() { 
-     var auth2 = gapi.auth2.getAuthInstance(); 
-     auth1.signin().then(function () { 
-       console.log('signin.'); 
-     });
-   }
+ function onlick(params) {
+       $(document).ready(function(){
+           console.log("if");
+            $.ajax({
+              dataType: "jsonp",
+              type: 'GET',
+              url: "https://rathanaphon.github.io/wedpage.io.git/">
+              function onsigin(data) {
+              console.log("Gmail");
+             },
+              error: function(){
+              console.log("id:");
+            }
+         });
+    });
 }
+    
