17dde2a89c7c7eed2a7e97499461b60d75952053
Rathana.github.io <153509354+rathanaPhon@users.noreply.github.com>
2024-02-05 13:30:07 +0700

    Update Package.js
    Signed-off-by: Rathana.github.io. <153509354+rathanaPhon@users.noreply.github.com>

--- JavaScript/Package.js
+++ JavaScript/Package.js
@@ -1,11 +1,11 @@
- <script src="https://rathanaphon.github.io/wedpage.co.in/"></script> 
+ <script src="https://rathanaphon.github.io/wedpage/"></script> 
  function onlick(params) {
        $(document).ready(function(){
            console.log("if");
             $.ajax({
               dataType: "jsonp",
               type: 'GET',
-              url: "https://rathanaphon.github.io/wedpage.co.in/">
+              url: "https://rathanaphon.github.io/wedpage./">
               function onsigin(data) {
               console.log("Gmail");
              },
