43a8587316ac93516894762c2c9f254ec81a729c
Rathana.github.io <153509354+rathanaPhon@users.noreply.github.com>
2024-02-08 05:27:41 +0700

    Create Node.js
    Signed-off-by: Rathana.github.io. <153509354+rathanaPhon@users.noreply.github.com>

---
+++ Node.js
@@ -0,0 +1,17 @@
+const xhrButton = document.querySelector(".xhr");
+const log = document.querySelector(".event-log");
+const url= "https://httpsrathanaphon.website.co.in">
+function handleEvent(e) {
+  log.content = `${log}
+}
+
+function addListeners(xhr) {
+}
+xhrButton.addEventListener("click", () => {
+  console.log.(message)="";
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    addListeners(xhr);
+    xhr.send();    
+ });
