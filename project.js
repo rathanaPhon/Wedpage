bc8be33498c6bd8e226bf8dac8175fe4bddb39e0
Rathana.github.io <153509354+rathanaPhon@users.noreply.github.com>
2024-02-08 08:42:03 +0700

    Website71801

---
+++ project.js
@@ -0,0 +1,20 @@
+{
+    "parent": "website71801"
+
+}
+{
+    "project":[]
+        {
+           "name": "projects/id:300922977015",
+           "parent": "Website/71801",
+           "projectId": "Cambodia/Website71801",
+           "state": "ACTIVE",
+           "displayName": "Cambodia"
+           "url": "https://rathanaphon.gihub.io/webpage/">
+        }
+    ]
+}
+    "files.exclude" (){
+    "**/*.js":( "when": "$(basename).ts" }
+    "**/**.js":( "when": "$(basename).tsx" }
+}
