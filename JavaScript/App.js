c808cf4f7c0b9402446927e63443108dfc33d269
Rathana.github.io <153509354+rathanaPhon@users.noreply.github.com>
2024-03-08 01:13:57 +0700

    Create App.js
    Signed-off-by: Rathana.github.io. <153509354+rathanaPhon@users.noreply.github.com>

---
+++ JavaScript/App.js
@@ -0,0 +1,30 @@
+{
+ result: [
+  {
+    "new_site_id": integer,
+     "new_url": "https://www.yourdomain.com/",
+     "url": "https://www.yourdomain.com/",
+     "messages": [
+   	 ""
+   ],
+     "errors": [
+   	"Entry for this domain already exists in your account"
+   ]
+ },
+ ...
+ ]
+   }
+{
+ entries: [
+  {
+   "id": "38275",
+   "url": "https://www.yourdomain.com/",
+   "created": 1515046557,
+   "pro_account": true/false,
+   "sitemap_updated": 1515046557,
+   "pages_indexed": 20,
+ },
+ ...
+ ],
+ total_entries: 80
+}
