ad544e4b79565624bb91a4036f5be59c7c1e7dea
Rathana.github.io <153509354+rathanaPhon@users.noreply.github.com>
2024-04-01 04:28:10 +0700

    Update index.js
    Signed-off-by: Rathana.github.io. <153509354+rathanaPhon@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const { styleText } = require('node:util');
-const errorMessage = styleText('red', 'Error! Error!');
-console.log(errorMessage);
+new PerformanceObserver((entryList) => {
+  const [pageNav] = entryList.getEntriesByType('navigation');
+
+  console.log(`TTFB: ${pageNav.responseStart}`);
+}).observe({
+  type: 'navigation',
+  buffered: true
+});
